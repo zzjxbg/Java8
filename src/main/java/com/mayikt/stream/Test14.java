a4821213bb8d47ca3b1b1cdcbf04584969b9bd8a
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-08-06 16:19:23 +0800

    stream过滤器的用法

---
+++ src/main/java/com/mayikt/stream/Test14.java
@@ -0,0 +1,23 @@
+package com.mayikt.stream;
+
+import com.mayikt.entity.UserEntity;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Test14 {
+    public static void main(String[] args) {
+        ArrayList<UserEntity> userEntities = new ArrayList<UserEntity>();
+        userEntities.add(new UserEntity("mayikt",20));
+        userEntities.add(new UserEntity("meite",28));
+        userEntities.add(new UserEntity("zhangsan",35));
+        Stream<UserEntity> stream = userEntities.stream();
+        stream.filter(new Predicate<UserEntity>() {
+            @Override
+            public boolean test(UserEntity userEntity) {
+                return "mayikt".equals(userEntity.getUserName()) && userEntity.getAge() > 18;
+            }
+        }).forEach((userEntity -> System.out.println(userEntity)));
+    }
+}
