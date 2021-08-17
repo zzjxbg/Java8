d8101fdd25bc3b3bc5882cf9455624011f673f70
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-08-17 17:45:42 +0800

    Optional判断对象是否为空

---
+++ src/main/java/com/mayikt/Test019.java
@@ -0,0 +1,18 @@
+package com.mayikt;
+
+import java.util.Optional;
+
+public class Test019 {
+    public static void main(String[] args) {
+        String userName = null;
+        /**
+         * ofNullable 可以允许传递一个空值对象
+         * of 不允许传递一个空值对象
+         */
+        Optional<String> optional = Optional.ofNullable(userName);
+//        Optional.of(userName);
+        boolean present = optional.isPresent();
+        //返回false表示传入值为空,true表示非空
+        System.out.println(present);
+    }
+}
